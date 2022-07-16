6340111ede24c740e922ecb9b4c3286498af51b8
Pedro Medina Faria <pedromedina190@gmail.com>
2022-07-16 19:11:19 -0400

    typing err

--- constants/multicall/index.ts
+++ constants/multicall/index.ts
@@ -1,7 +1,7 @@
 import { ChainId } from "@pangolindex/sdk";
 import MULTICALL_ABI from "./abi.json";
 
-const MULTICALL_NETWORKS: { [chainId in ChainId]: string } = {
+const MULTICALL_NETWORKS: { [chainId in ChainId]?: string } = {
   [ChainId.FUJI]: "0xb465Fd2d9C71d5D6e6c069aaC9b4E21c69aAA78f",
   [ChainId.AVALANCHE]: "0x0FB54156B496b5a040b51A71817aED9e2927912E"
 };
