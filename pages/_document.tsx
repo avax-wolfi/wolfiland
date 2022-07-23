a45117975407c7724f392390782081194e20a12c
darwcode9 <email@example.com>
2022-07-22 22:56:19 -0400

    Fix some issues

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,24 +4,25 @@ export default function Document() {
   return (
     <Html>
       <Head>
-    <div>
+
   {/* Primary Meta Tags */}
-  <title>Welcome to Wolfiland!</title>
-  <meta name="title" content="Welcome to Wolfiland!" />
-  <meta name="description" content="I am Mar, the creator of wolfi, the most popular mascot among the Avalanche community. I’ve been active working on different projects, but currently, I am a freelancer designer. I created wolfi after a group of friends that loved AVAX asked me to do it. Today, I decided to create the first official wolfi collection, which mainly works as a fund to support me as an artist." />
+  <title>Wolfi Land</title>
+  <meta name="title" content="Wolfi Land" />
+  <meta name="description" content="Welcome to the first official NFT collection of Wolfi! The most popular mascot of Avalanche (AVAX). Enter now and mint your own Wolfi!" />
   {/* Open Graph / Facebook */}
   <meta property="og:type" content="website" />
-  <meta property="og:url" content="https://metatags.io/" />
-  <meta property="og:title" content="Welcome to Wolfiland!" />
-  <meta property="og:description" content="I am Mar, the creator of wolfi, the most popular mascot among the Avalanche community. I’ve been active working on different projects, but currently, I am a freelancer designer. I created wolfi after a group of friends that loved AVAX asked me to do it. Today, I decided to create the first official wolfi collection, which mainly works as a fund to support me as an artist." />
-  <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+  <meta property="og:url" content="https://www.wolfi.land" />
+  <meta property="og:title" content="Wolfi Land" />
+  <meta property="og:description" content="Welcome to the first official NFT collection of Wolfi! The most popular mascot of Avalanche (AVAX). Enter now and mint your own Wolfi!" />
+  <meta property="og:image" content="https://www.wolfi.land/public/img/og-wolfi.jpg" />
   {/* Twitter */}
   <meta property="twitter:card" content="summary_large_image" />
-  <meta property="twitter:url" content="https://metatags.io/" />
-  <meta property="twitter:title" content="Welcome to Wolfiland!" />
-  <meta property="twitter:description" content="I am Mar, the creator of wolfi, the most popular mascot among the Avalanche community. I’ve been active working on different projects, but currently, I am a freelancer designer. I created wolfi after a group of friends that loved AVAX asked me to do it. Today, I decided to create the first official wolfi collection, which mainly works as a fund to support me as an artist." />
-  <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
-</div>
+  <meta property="twitter:url" content="https://www.wolfi.land" />
+  <meta property="twitter:title" content="Wolfi Land" />
+  <meta property="twitter:description" content="Welcome to the first official NFT collection of Wolfi! The most popular mascot of Avalanche (AVAX). Enter now and mint your own Wolfi!" />
+  <meta property="twitter:image" content="https://www.wolfi.land/public/img/og-wolfi.png" />
+
+
 
 
       </Head>
