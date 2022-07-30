48504fa1667fd8521dc5004057ca889d48be15ee
darwcode9 <email@example.com>
2022-07-30 15:05:10 -0400

    Fix background Welcome

--- components/Welcome/Welcome.tsx
+++ components/Welcome/Welcome.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Image from 'next/image'
 import mint from '../../public/icons/mint now 1.png'
 import mintSm from '../../public/img/mint now 1-sm.svg'
-
+import wolfilandSm from '../../public/img/Logo-sm.svg'
 import styles from './Welcome.module.css'
 import Pad from '../Pad'
 
@@ -12,6 +12,14 @@ export default React.memo<WelcomeProps>(function Welcome() {
   return (
     <div className={styles['container']}>
       <div className={styles['welcome']}>
+
+        <div className={styles['welcome-sm']}>
+        <h1 style={{fontSize:'20px'}}>welcome to</h1>
+
+        <div className={styles['wolfiland-sm']}>
+          <Image src={wolfilandSm} />
+        </div>
+        </div>
         <div className={styles['left-box']}></div>
       
         <div className={styles['right-box']}>
