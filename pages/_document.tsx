8f3ba85e1d994bd68e0fb664d3d67218c7656012
Pedro Medina Faria <pedromedina190@gmail.com>
2022-08-13 10:43:40 -0400

    og fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,20 +22,14 @@ export default function Document() {
         />
         <meta
           property="og:image"
-          content="/img/og-wolfi.png"
+          content="img/og-wolfi.png"
         />
         {/* Twitter */}
         <meta property="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@wolfilandnfts" />
+        <meta name="twitter:creator" content="@HermanaRobot" />
         <meta property="twitter:url" content="https://www.wolfi.land" />
         <meta property="twitter:title" content="Wolfi Land" />
-        <meta
-          property="twitter:description"
-          content="Welcome to the first official NFT collection of Wolfi! The most popular mascot of Avalanche (AVAX). Enter now and mint your own Wolfi!"
-        />
-        <meta
-          property="twitter:image"
-          content="/img/og-wolfi.png"
-        />
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
